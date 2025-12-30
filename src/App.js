269d3f6244d4e2cbe47b624ff88be7ba5fd8c460
Oleh Pihnastyi <pihnastyi@gmail.com>
2025-12-30 20:13:33 +0100

    L11 Grid

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
-import logo from './logo.svg';
-import './App.css';
-import Header from './less09/Header';
-import Technologies from './less09/Technologies';
 
-const App =() => {
+
+const App = () => {
   return (
-    <div className="App">
-      <div><Header /></div>
-      <div><Technologies /></div>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxMOQxU2IdDkXh_4DnrcAsfpR7UCrIozgMkg&s' />
+      </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe_OlHEe2L-0VtexCxjIV5tc0dLor3wd57Yg&s' />
+        </div>
+        <div>ava+desc </div>
+        <div>My posts
+          <div>New post
+            <div>Post 1</div>
+            <div>Post 2</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/less09/Footer.js
@@ -0,0 +1,11 @@
+const Footer =() => {
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      <a>Home</a>
+      <a>Home</a>
+    </div>
+  );
+}
+
+export default Footer;
