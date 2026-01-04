266fbdf2b2adef0dc97e8634b9ca8a5a331b0ab9
Oleh Pihnastyi <pihnastyi@gmail.com>
2026-01-04 21:06:20 +0100

    L16 Props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -12,12 +12,8 @@ const MyPosts = () => {
         <button>Remove</button>
       </div>
       <div className={classes.posts}>
-        <Post />
-        <Post />        
-        <Post />        
-        <Post />        
-        <Post />
-
+        <Post message='Hi, how are you?'/>
+        <Post message="It's my first post"/>        
       </div>
     </div>
   )
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,11 +1,13 @@
 import classes from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
+  // console.log(props.message);
+  // debugger;
   return (
     <div className={classes.item}>
       <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYHoyQNil3iW7vA-Ryc2IwTJtq-9bWqWfE_g&s' />
-      Post 1
       <div>
+        {props.message}
         <span>Like</span>
       </div>
     </div>
