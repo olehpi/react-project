92d3c36772f01e328abeb6a06ef6ec5099af2c50
Oleh Pihnastyi <pihnastyi@gmail.com>
2026-02-02 15:28:47 +0100

    L75 Login is added using Redux-final-form lbrary

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,3 +1,43 @@
-const login = () => { return <h1>LOGIN</h1> }
+import { Form, Field } from "react-final-form";
 
-export default login;
+const LoginForm = ({ handleSubmit }) => {
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <Field placeholder="Login" name="login" component="input" />
+            </div>
+            <div>
+                <Field name="password">
+                    {({ input }) => <input {...input} type="password" placeholder="Password" />}
+                </Field>
+            </div>
+            <div>
+                <Field type="checkbox" name="rememberMe" component="input" /> Remember me
+            </div>
+            <div>
+                <button type="submit">Login</button>
+            </div>
+        </form>
+    );
+};
+
+const LoginReactFinalForm = ({ onSubmit }) => {
+    return <Form onSubmit={onSubmit} render={({ handleSubmit }) => (<LoginForm handleSubmit={handleSubmit} />)} /> 
+};
+
+const Login = () => {
+    const onSubmit = (formData) => { console.log(formData); };
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginReactFinalForm onSubmit={onSubmit} />
+        </div>
+    );
+};
+
+export default Login;
+
+/* // Alternative way to define password field to process its errors
+<Field placeholder={"Password"} name={"password"} component="input" type="password" />
+*/
