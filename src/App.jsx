60cbebed07dd2b1c01bbb0951a77a83cdda21d41
Oleh Pihnastyi <pihnastyi@gmail.com>
2026-02-15 16:05:58 +0100

    L94 Lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
+import React,  { Suspense } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import { Routes, Route } from 'react-router-dom';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import LoginPage from './components/login/login';
 import { connect } from "react-redux";
@@ -12,6 +10,10 @@ import { withRouter } from './components/Profile/withRouter';
 import { compose } from 'redux';
 import { initializeApp } from './store/app-reducer';
 import Preloader from './components/common/Preloader/Preloader';
+import { withSuspense } from './hoc/withSuspense';
+
+const DialogsContainer = React.lazy( () => import('./components/Dialogs/DialogsContainer') );
+const ProfileContainer = React.lazy( () => import('./components/Profile/ProfileContainer') );
 
 class App extends React.Component {
   componentDidMount() {
@@ -23,17 +25,22 @@ class App extends React.Component {
       return <Preloader />;
     }
 
+    const DialogsWithSuspense = withSuspense(DialogsContainer);
+    const ProfileWithSuspense = withSuspense(ProfileContainer);
+
     return (
       <div className="app-wrapper">
         <HeaderContainer />
         <Navbar />
         <div className="app-wrapper-content">
-          <Routes>
-            <Route path="/dialogs/*" element={<DialogsContainer />} />
-            <Route path="/profile/:userId?" element={<ProfileContainer />} />
-            <Route path="/users" element={<UsersContainer />} />
-            <Route path="/login" element={<LoginPage />} />
-          </Routes>
+          {/*<Suspense fallback={<Preloader />}> */}
+            <Routes>
+              <Route path="/dialogs/*" element={<DialogsWithSuspense/>} />
+              <Route path="/profile/:userId?" element={<ProfileWithSuspense/>} />
+              <Route path="/users" element={<UsersContainer />} />
+              <Route path="/login" element={<LoginPage />} />
+            </Routes>
+          {/*</Suspense> */}
         </div>
       </div>
     );
---
+++ src/hoc/withSuspense.jsx
@@ -0,0 +1,10 @@
+import Preloader from "../components/common/Preloader/Preloader";
+import { Suspense } from "react";
+
+export const withSuspense = (Component) => {
+    return (props) => {
+        return <Suspense fallback={<Preloader />} >
+            <Component {...props} />
+        </Suspense>
+    };
+}
