79890b0abd46bd96c5684f2818cb72b402f18191
Oleh Pihnastyi <pihnastyi@gmail.com>
2026-04-05 13:08:58 +0200

    Updates

--- assets/withAuthRedirect-7Sh1EWfB.js
+++
@@ -1,1 +0,0 @@
-import{o as u,j as t,p as i}from"./index-lmXgGD3a.js";const n=e=>r=>u(s=>s.auth.isAuth)?t.jsx(e,{...r}):t.jsx(i,{to:"/login",replace:!0});export{n as w};
---
+++ assets/withAuthRedirect-DxtZb4uE.js
@@ -0,0 +1,1 @@
+import{o as u,j as t,p as i}from"./index-Dprr973x.js";const n=e=>r=>u(s=>s.auth.isAuth)?t.jsx(e,{...r}):t.jsx(i,{to:"/login",replace:!0});export{n as w};
